14aca0f7815f0351aa20b3b57cd6c07ead610238
strzesia <kmatelowska@gmail.com>
2020-11-26 16:59:50 +0100

    fix rubocop offences

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,7 +26,6 @@ SimpleCov.start "rails"
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
