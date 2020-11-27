2867cef7963f305cef4181c588b2572a475700af
strzesia <kmatelowska@gmail.com>
2020-11-27 19:40:01 +0100

    write specs

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'validated uniqueness of movie in user scope' do
+    let(:user) { create(:user) }
+    let(:movie) { create(:movie) }
+    let(:comment) { Comment.new(text: 'text', user: user, movie: movie) }
+
+    context "when user already commented on movie" do
+      before do
+        create(:comment, user: user, movie: movie)
+      end
+
+      it "does not add comment if one exists" do
+        expect(comment).not_to be_valid
+      end
+    end
+
+    context "when user haven't commented on movie yet" do
+      it "does not add comment if one exists" do
+        expect(comment).to be_valid
+      end
+    end
+  end
+end
